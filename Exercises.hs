054329a9bbe12129cdc03a50c460e294af40a5d5
petrapoklukar <petra.poklukar@gmail.com>
2015-01-08 13:59:27 +0100

    Implemented and tested first exercise.

--- Exercises.hs
+++ Exercises.hs
@@ -22,16 +22,29 @@ existsT :: T -> (Baire -> Bool) -> Bool
 
 existsT t p = p(findT t p)
 
-shiftPredicate :: (Baire->Bool) -> (Baire -> Bool)
-shiftPredicate p i = \sequence -> p (\n -> sequence (n + i))
+shiftPredicate :: (Baire->Bool)-> N -> (Baire -> Bool)
+shiftPredicate p x = \sequence -> p (\n -> if n == 0 then x else sequence (n-1))
 
-findTaux (Fork [s]) p 0 = let (i, tree) = s in i
-findTaux (Fork [s]) p n = let (i, tree) = s in findTaux tree (shiftPredicate p 1) (n-1)
-findTaux (Fork x:xs) p n = let (i, tree) = x in
-    if p (\i -> findTaux (Fork [x]) p i )  then i
-       else findTaux (Fork xs) p n
+
+findTaux (Fork ((i, tree) : [])) p = \n -> if n == 0 then i else findTaux tree (shiftPredicate p i) (n-1)
+findTaux (Fork ((i, tree) : xs)) p = let solution = findTaux (Fork ((i, tree) : [])) p
+                                     in \n -> if p solution then solution n
+                                              else findTaux (Fork xs) p n
 
 findT = findTaux
 
-predicate :: ( Baire -> Bool)
-predicate b = b 0 == 0
+pr :: ( Baire -> Bool)
+pr b = b 12 == 1
+
+myTree :: N ->  T
+myTree n = Fork [(n,myTree (n+1))]
+mt = myTree 0
+
+-- Example.
+a = findT cantorTree pr
+
+-- Naloga 2:
+findT'    :: T -> (Baire -> Bool) -> Baire
+forsomeT' :: T -> (Baire -> Bool) -> Bool
+
+forsomeT' t p = p(findT' t p)
